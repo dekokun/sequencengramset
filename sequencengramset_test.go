a87a3d4bc6a8ff47e927f1d0f7ba933b6f02243f
dekokun <dekokun@hatena.ne.jp>
2016-10-05 21:40:40 +0900

    重複した文字列テスト追加

--- sequencengramset_test.go
+++ sequencengramset_test.go
@@ -43,6 +43,14 @@ func Test0は受け付けない(t *testing.T) {
 	}
 }
 
+func Test重複した文字列があっても大丈夫(t *testing.T) {
+	expectSet := map[string]struct{}{"あい": struct{}{}, "いう": struct{}{}, "うえ": struct{}{}, "えあ": struct{}{}}
+
+	if set, _ := Sequencengramset("あいうえあいうえ", 2); !reflect.DeepEqual(set, expectSet) {
+		t.Errorf("expected %d to eq %d", set, expectSet)
+	}
+}
+
 func Benchmark(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		Sequencengramset("ABCDEFGFALKJLAKJDLKJFLKSDJFLKSJFLSKJDFLKSJFLKKDSJFLSJDFLSKDJFKLSJDFKLSJFLKDKJFLKSDJLFKSDJFLDSKJFLDSJLKSJDFLKJDSFD", 2)
